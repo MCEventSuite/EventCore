f9787aca94c4fc817b492e01b2fbffa56917229f
Stuart Pomeroy <rushmead@gmail.com>
2020-11-09 03:17:43 +0000

    Add InfluxDB

--- src/main/java/dev/imabad/mceventsuite/core/config/database/InfluxConfig.java
+++ src/main/java/dev/imabad/mceventsuite/core/config/database/InfluxConfig.java
@@ -1,5 +1,28 @@
 package dev.imabad.mceventsuite.core.config.database;
 
-public class InfluxConfig {
+import dev.imabad.mceventsuite.core.api.BaseConfig;
+import dev.imabad.mceventsuite.core.api.database.BaseDatabaseConfig;
 
+public class InfluxConfig extends BaseConfig {
+
+    private String url;
+    private String org;
+    private String bucket;
+
+    @Override
+    public String getName() {
+        return "influx";
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getOrg() {
+        return org;
+    }
+
+    public String getBucket() {
+        return bucket;
+    }
 }
---
+++ src/main/java/dev/imabad/mceventsuite/core/modules/influx/InfluxDBModule.java
@@ -0,0 +1,75 @@
+package dev.imabad.mceventsuite.core.modules.influx;
+
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.InfluxDBClientFactory;
+import com.influxdb.client.WriteApi;
+import com.influxdb.client.internal.InfluxDBClientImpl;
+import com.influxdb.client.write.Point;
+import dev.imabad.mceventsuite.core.api.IConfigProvider;
+import dev.imabad.mceventsuite.core.api.modules.Module;
+import dev.imabad.mceventsuite.core.config.database.InfluxConfig;
+
+import java.util.Collections;
+import java.util.List;
+
+public class InfluxDBModule extends Module implements IConfigProvider<InfluxConfig> {
+
+    private InfluxDBClient client;
+    private InfluxConfig config;
+
+    @Override
+    public String getName() {
+        return "influx";
+    }
+
+    @Override
+    public void onEnable() {
+        client = InfluxDBClientFactory.create(config.getUrl());
+    }
+
+    @Override
+    public void onDisable() {
+        client.close();
+    }
+
+    @Override
+    public List<Class<? extends Module>> getDependencies() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Class<InfluxConfig> getConfigType() {
+        return InfluxConfig.class;
+    }
+
+    @Override
+    public InfluxConfig getConfig() {
+        return config;
+    }
+
+    @Override
+    public String getFileName() {
+        return "influx.json";
+    }
+
+    @Override
+    public void loadConfig(InfluxConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public void saveConfig() {
+
+    }
+
+    @Override
+    public boolean saveOnQuit() {
+        return false;
+    }
+
+    public void writePoints(List<Point> dataPoints){
+        try(WriteApi writeApi = client.getWriteApi()){
+            writeApi.writePoints(dataPoints);
+        }
+    }
+}
