583493ebd3772da28983ce9eb15e662613f3ba64
Ben Houghton <bennixerus@gmail.com>
2022-10-10 15:56:10 +0100

    Add bedrock versions of ranks

--- src/main/java/dev/imabad/mceventsuite/core/util/SpecialTag.java
+++ src/main/java/dev/imabad/mceventsuite/core/util/SpecialTag.java
@@ -1,22 +1,35 @@
 package dev.imabad.mceventsuite.core.util;
 
 public enum SpecialTag {
-    CHAT(0x0316),
-    DEV(0x0301),
-    FOUNDER(0x0302),
-    AFK(0x0303),
-    EXHIBITOR(0x0304),
-    GAMEMASTER(0x0305),
-    GUEST(0x0306),
-    HEAD(0x0307),
-    LIVESTREAM(0x0308),
-    MANAGER(0x0309),
-    MOD(0x0310),
-    SPONSOR(0x0311),
-    SRMOD(0x0312),
-    STICKYPISTON(0x0313),
-    VIP(0x0314),
-    VIPP(0x0315);
+    CHAT(0x0316, 0xE039),
+    DEV(0x0301, 0xE031),
+    FOUNDER(0x0302, 0xE033),
+    AFK(0x0303, 0xE030),
+    EXHIBITOR(0x0304, 0xE032),
+    GAMEMASTER(0x0305, 0xE034),
+    GUEST(0x0306, 0xE035),
+    HEAD(0x0307, 0xE036),
+    LIVESTREAM(0x0308, 0xE037),
+    MANAGER(0x0309, 0xE038),
+    MOD(0x0310, 0xE019),
+    SPONSOR(0x0311, 0xE01A),
+    SRMOD(0x0312, 0xE01B),
+    STICKYPISTON(0x0313, 0xE01C),
+    VIP(0x0314, 0xE01D),
+    VIPP(0x0315, 0xE01E),
+
+    //BOSSBAR
+    NORTH(0x0317),
+    EAST(0x0318),
+    SOUTH(0x0319),
+    WEST(0x0320),
+    NORTHEAST(0x0321),
+    SOUTHEAST(0x0322),
+    SOUTHWEST(0x0323),
+    NORTHWEST(0x0324),
+
+    LOCATION_PIN(0x0333),
+    CLOCK(0x0334);
 
     private char java;
     private char[] bedrock;
